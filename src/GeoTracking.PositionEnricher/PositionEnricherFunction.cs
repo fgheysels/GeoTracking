acea9203f7283cf95b18aff507e08de18ae4ba76
Frederik Gheysels <frederik.gheysels@codit.eu>
2018-12-23 19:32:42 +0100

    Renamed Function to PositionEnricherFunction

--- src/GeoTracking.PositionEnricher/PositionEnricherFunction.cs
+++ src/GeoTracking.PositionEnricher/PositionEnricherFunction.cs
@@ -4,9 +4,11 @@ using Microsoft.Extensions.Logging;
 
 namespace GeoTracking.PositionEnricher
 {
-    public static class Function1
+    public static class PositionEnricherFunction
     {
-        [FunctionName("Function1")]
+        private const string FunctionName = "PositionEnricher";
+
+        [FunctionName(FunctionName)]
         public static void Run([EventHubTrigger("samples-workitems", Connection = "")]string myEventHubMessage, ILogger log)
         {
             log.LogInformation($"C# Event Hub trigger function processed a message: {myEventHubMessage}");
