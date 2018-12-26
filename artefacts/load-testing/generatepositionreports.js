d2104fbe4e5b7ed77954e0a3f27450930e98f56c
Frederik Gheysels <frederik.gheysels@codit.eu>
2018-12-26 18:40:07 +0100

    Add load-testing scripts for usage with Artillery

---
+++ artefacts/load-testing/generatepositionreports.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+    generateRandomData
+};
+
+// Make sure to "npm install faker" first.
+const Faker = require('faker');
+
+function generateRandomData(userContext, events, done) {
+    // generate data with Faker:
+   
+    const shipId = Faker.random.number(2000);
+    const timestamp = Faker.date.recent(7);
+    const longitude = Faker.random.number({min: -180, max: 180, precision: 0.000001});
+    const latitude = Faker.random.number({min: -90, max: 90, precision: 0.000001});
+
+    // add variables to virtual user's context:
+    userContext.vars.shipId = shipId;
+    userContext.vars.timestamp = timestamp;
+    userContext.vars.longitude = longitude;
+    userContext.vars.latitude = latitude;
+    // continue with executing the scenario:
+    return done();
+}
