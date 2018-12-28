4c1925eb5a1872482b2bdf164882cd1522037833
Frederik Gheysels <frederik.gheysels@codit.eu>
2018-12-28 21:46:25 +0100

    HTTP Feed Function

---
+++ src/GeoTracking.HttpFeed/GeoTrackingFeedFunction.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace GeoTracking.HttpFeed
+{
+    public static class GeoTrackingFeedFunction
+    {
+        [FunctionName("GeoTrackingFeed")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [EventHub("%eventHubName%", Connection = "eventHubConnection")]out EventData outputData,
+            ILogger log)
+        {
+            outputData = null;
+
+            if (req.Method.Equals("GET", StringComparison.OrdinalIgnoreCase))
+            {
+                return new OkObjectResult("HTTP feed online");
+            }
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            var data = JsonConvert.DeserializeObject<PositionReport>(requestBody);
+
+            if (data == null)
+            {
+                return new BadRequestObjectResult("JSON body not in expected format.");
+            }
+
+            outputData = new EventData(System.Text.Encoding.UTF8.GetBytes(requestBody));
+
+            return new AcceptedResult();
+        }
+    }
+
+    public class PositionReport
+    {
+        public long ShipId { get; set; }
+        public string Source { get; set; }
+        public DateTime Timestamp { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
