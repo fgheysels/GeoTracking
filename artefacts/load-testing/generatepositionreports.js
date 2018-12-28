30f377f0aa6b000ca995ee2f8712b99de611cace
Frederik Gheysels <frederik.gheysels@codit.eu>
2018-12-28 21:52:28 +0100

    Modified load-test script to include Source (Ais, Vms, Lrit)

--- artefacts/load-testing/generatepositionreports.js
+++ artefacts/load-testing/generatepositionreports.js
@@ -14,12 +14,14 @@ function generateRandomData(userContext, events, done) {
     const timestamp = Faker.date.recent(7);
     const longitude = Faker.random.number({min: -180, max: 180, precision: 0.000001});
     const latitude = Faker.random.number({min: -90, max: 90, precision: 0.000001});
+    const source = Faker.random.arrayElement(["Ais", "Vms", "Lrit"]);
 
     // add variables to virtual user's context:
     userContext.vars.shipId = shipId;
     userContext.vars.timestamp = timestamp;
     userContext.vars.longitude = longitude;
     userContext.vars.latitude = latitude;
+    userContext.vars.source = source;
     // continue with executing the scenario:
     return done();
 }
